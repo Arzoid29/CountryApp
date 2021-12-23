a8fda13de1c219e5b24ce475549dc7de371948aa
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2021-12-23 10:25:28 -0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import {useEffect} from "react"
 
 function App() {
-  useEffect(fetch("https://restcountries.com/v3.1/all").then(res => {
-    if(res.ok == true){
-      res.json() .then(data => {
-        console.log(data)
-      })
-    }
-  }),[])
-  
-
   return (
     <div className="App">
+      <h1>Hola</h1>
     </div>
   )
 }
