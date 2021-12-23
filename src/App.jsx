fce235ed4da22d1722bfcfee213858ee8eccc123
Nomar Gomez Cabreja <ngomez@intellisys.com.do>
2021-12-23 13:28:39 -0400

    Fix fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import {useEffect} from "react"
+import { useEffect, useState } from "react"
+import { render } from "react-dom"
 
 function App() {
+
+  const [data, setData] = useState("")
+  
+  useEffect(() => fetch("https://restcountries.com/v3.1/all").then(res => {
+    if(res.ok == true){
+      res.json() .then(dataJSON => {
+        setData(dataJSON)
+      })
+    }
+  }), [])
+
+  console.log(data)
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <h1>Working</h1>
     </div>
   )
 }
